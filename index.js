4ce794480f3513f7082a522de8249f720b166f8c
Felicia Melany <feliciamelany01@gmail.com>
2025-09-08 12:30:36 -0300

    Reajustando

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-import { rl } from "./readlineInterface"; //importanto função readline para usar no meu arquivo index.js
-import { estudantes } from "./studentRegistration"; //importanto array de estudantes
+import { rl } from "./readlineInterface.js"; //importanto função readline para usar no meu arquivo index.js
+import { cadastroEstudantes, estudantes } from "./studentRegistration.js"; //importanto array de estudantes
 
-const estudantes = [];
 
 const questionStudents = () =>{
     rl.question('Digite o nome do estudante: ', nome =>{
-    })
-    rl.question('Digite a idade do estudante: ', idade =>{
-    })
-    rl.question('Digite as notas do estudante mediante aos 4° semestres: ', notas => {
-    })
-    rl.question('Deseja cadastrar outro estudante (s/n): ', answer => {  
-    } )
-}
+            
+        rl.question('Digite a idade do estudante: ', idadeStr =>{
+                const idade = Number(idadeStr)
+
+            rl.question('Digite as notas do estudante mediante aos 4 semestres: ', notasStr =>{
+                const notas = notasStr.split(',').map(n => Number(n.trim()));
+
+                cadastroEstudantes(nome, idade, notas);
+                console.log(estudantes);
+            
+                rl.question('Deseja cadastrar outro estudante? (s/n): ', answer =>{
+                    if( answer === 's'){
+                        console.log('-----------------------------------------------------')
+                        questionStudents();
+                    };
+                    rl.close()
+                });
+            });
+        });
+    });
+};
+questionStudents();
+
